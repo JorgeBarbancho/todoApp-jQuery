a0cc8581ff5ce7f1a92d6a7774f80bfbafabf525
Jorge Barbancho <jorge.barbancho@kps.com>
2021-12-02 10:26:45 +0100

    finish first version

--- todoApp.js
+++ todoApp.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    let todoList = [];
-    let taskStatus = [];
+    let pendingTasks = 0;
 
+    // Add button
     $('#add-button').click(function () {
         if ($('#text').val().length !== 0) {
+            let currentList = $('.todo-component__todo-list').html();
             let newTodo = `<div class="todo-component__todo-item">
                                 <input type="checkbox" id="checkbox">
                                 <p class="todo-component__todo-item-text"> ` + $('#text').val() + `</p>
@@ -11,49 +12,73 @@ $(document).ready(function () {
                                     <i class="fas fa-trash todo-component__trash-icon"></i>
                                 </button>
                             </div>`;
-            todoList.push(newTodo);
-            taskStatus.push(false);
             $('#text').val("");
-            showAll();
+            $('.todo-component__todo-list').html(currentList + newTodo);
+            pendingTasks++;
+            updateTaskStatus();
         } else alert("Enter some Text!");
     });
 
+    // Clear button
     $('#clear-button').click(function () {
-        todoList = [];
-        taskStatus = [];
-        showAll();
-    });
-
-    $(document).on('click', '.todo-component__trash-icon-container', function () {
-        let task to
-        todoList.splice(todoList.indexOf($('.todo-component__trash-icon-container').parent()));
-        showAll();
-    });
-
-    $(document).on('click', '#checkbox', function () {
-        console.log("hola");
-    });
-
-    $('#show-all').click(function () {
-        showAll();
-    });
-
-    $('#show-completed').click(function () {
-        $('.todo-component__todo-list').html(taskStatus);
-    });
-
-    $('#show-pending').click(function () {
-        $('.todo-component__todo-list').html(todoList);
-    });
-
-    function showAll() {
+        console.log("test");
         $('.todo-component__todo-list').html("");
-        renderList(todoList);
-        $('.todo-component__pending-text').text(`You have ${todoList.length} pending tasks`);
-    }
+        pendingTasks = 0;
+        updateTaskStatus();
+    });
 
-    function renderList(list) {
-        list.forEach(task => $('.todo-component__todo-list').append(task));
+    // Remove button
+    $(document).on('click', '.todo-component__trash-icon-container', function () {
+        let taskToDelete = $(this).parent();
+        taskToDelete.remove();
+        pendingTasks--;
+        updateTaskStatus('remove')
+    });
+
+    // Show all button
+    $('#show-all').click(function () {
+        $('.todo-component__todo-list').children().each(function () {
+            $(this).show();
+        });
+    });
+
+    // Show completed button
+    $('#show-completed').click(function () {
+        $('.todo-component__todo-list').children().each(function () {
+            if ($(this).children('#checkbox').is(':checked')) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    // Show pending button
+    $('#show-pending').click(function () {
+        $('.todo-component__todo-list').children().each(function () {
+            if ($(this).children('#checkbox').is(':checked')) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    });
+
+    // Checkbox
+    $(document).on('click', '#checkbox', function () {
+        if ($(this).is(':checked')) {
+            pendingTasks--;
+        } else {
+            pendingTasks++;
+        }
+        updateTaskStatus();
+    });
+
+    // Update pending tasks
+    function updateTaskStatus() {
+        let taskText = 'tasks';
+        taskText = pendingTasks === 1 ? 'task' : 'tasks';
+        $('.todo-component__pending-text').text(`You have ${pendingTasks} pending ${taskText}`);
     }
 });
 
